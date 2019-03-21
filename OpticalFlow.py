2a8b6b617a4465be012f94d0a5fe305711d3dda4
fondachau <chau.fonda@gmail.com>
2019-03-21 00:12:20 -0700

    Update OpticalFlow.py

--- OpticalFlow.py
+++ OpticalFlow.py
@@ -11,8 +11,8 @@ from numpy import *
 
 
 Znet=0
-#cap = cv2.VideoCapture("outcpp2.avi")
-cap=cv2.VideoCapture(0)
+cap = cv2.VideoCapture("Ltest2.mp4")
+#cap=cv2.VideoCapture(0)
 ret, old_frame = cap.read()
 ## get first frame to compair
 oldframe_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
@@ -28,15 +28,18 @@ fig.show()
 t=[0]
 flow=float(0)
 n=65536
-
+lastbmp=0
+timenow=0
 while(1):
 
 	#print(time.process_time())
 	ret, curr = cap.read()
 	
-	curr=cv2.resize(curr,(int(width/scalefactor),int(height/scalefactor)))
+#	curr=cv2.resize(curr,(int(width/scalefactor),int(height/scalefactor)))
 
 	frame_gray = cv2.cvtColor(curr, cv2.COLOR_BGR2GRAY)
+	frame_gray=cv2.resize(frame_gray,(int(width/scalefactor),int(height/scalefactor)))
+
 	#cv2.imshow('frame',old_frame)
 	
 	displacementy=[0]
@@ -44,27 +47,30 @@ while(1):
 	oldframe_gray = frame_gray
 
 	#print(flow)
-	for x in range(int(300/scalefactor), int(500/scalefactor),1):
-			for y in range(int(300/scalefactor),int(400/scalefactor),1):
+	for x in range(int(900/scalefactor), int(1100/scalefactor),1):
+			for y in range(int(600/scalefactor),int(900/scalefactor),1):
 				
 				Znet+= flow[y,x]
 				znow=flow[y,x]
-				cv2.line(curr,(x,y),( int(x), int(y+10*znow[1])),(255,0,0),2)
-				cv2.circle(curr,(x,y), 1, (0,0,255), -1)
+				cv2.line(curr,(x*scalefactor,y*scalefactor),( int(x*scalefactor), int(y*scalefactor+10*znow[1]*scalefactor)),(255,0,0),2)
+				cv2.circle(curr,(x*scalefactor,y*scalefactor), 1, (0,0,255), -1)
 	ynet=Znet[1]
-	if (len(motion)>100):
+	print(motion)
+	if (len(motion)>200):
 		motion=motion[1:]
 		t=t[1:]
 		
 		
 		spectrum = fft.fft(motion,n=n)
+		print(spectrum)
 		spectrum=abs(spectrum)
 		freq = fft.fftfreq(len(spectrum),d=1/Fs)
 
-		rangepoint1Hz=int(0.2/(Fs/n))
+		rangepoint1Hz=int(.15/(Fs/n))
 		#print(rangepoint1Hz)
 		range3hz=int(3/(Fs/n))
-		
+		#print(freq[rangepoint1Hz])
+		#print(Fs)
 		#print(range3hz)
 		freq=freq[rangepoint1Hz:range3hz]
 		spectrum=spectrum[rangepoint1Hz:range3hz]
@@ -88,20 +94,22 @@ while(1):
 		#plt.show()
 	motion.append(-ynet)
 	
-	
-	t.append(time.process_time())
-	
-	Fs=1/((t[len(t)-1]-t[0])/(len(t)-1))
+	timenow=timenow+1/30
+	#t.append(time.process_time())
+	t.append(timenow)
+	Fs=1/30
+	#Fs=1/((t[len(t)-1]-t[0])/(len(t)-1))
 	
 	
 	ax.plot(t, motion, color='b')
 	fig.canvas.draw()
 	i=t[len(t)-1]
-	ax.set_xlim(left=max(0, i-15), right=i+2)
+	ax.set_xlim(left=max(0, i-20), right=i+2)
 	ax.set_ylim(bottom=min(motion),top=max(motion))
 	
 	#print(motion)
 	cv2.imshow('frame',curr)
+	
 	k = cv2.waitKey(30) & 0xff
 	if k == 27:
 		break
